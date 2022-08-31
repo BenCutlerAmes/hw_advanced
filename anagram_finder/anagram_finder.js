3bd748cae27fe82d043eb661ba680c3726edbb95
Benjamin Cutler-Ames <bencutames@gmail.com>
2022-08-31 20:28:34 +0100

    anagram finder complete

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,20 @@
 const AnagramFinder = function (word) {
+    this.word = word.toLowerCase().split("").sort().join()
+    this.original = word
 
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+    const anagrams = []
+    otherWords.forEach((word)=>{
+        const sortedWord = word.toLowerCase().split("").sort().join()
+        if (this.word === sortedWord){
+            if(this.original !== word){
+                anagrams.push(word)
+            }
+        }
+    })
+    return anagrams
 }
 
 module.exports = AnagramFinder;
