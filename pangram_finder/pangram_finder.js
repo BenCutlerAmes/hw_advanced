3f720a21b2ff9770d7e4f2b9c085250808ec7286
Benjamin Cutler-Ames <bencutames@gmail.com>
2022-08-31 19:25:09 +0100

    pangram finder complete

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,13 @@
-const PangramFinder = function (phrase) {
+const PangramFinder = function (input) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.input = input.toLowerCase().split('')
 }
+
+
 
 PangramFinder.prototype.isPangram = function () {
-
+  return this.alphabet.every((letter)=> this.input.includes(letter))
 }
+
 
 module.exports = PangramFinder;
