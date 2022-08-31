a5a46ae18cf9657ccd0d9e17f6203cf7a7cbafe4
Benjamin Cutler-Ames <bencutames@gmail.com>
2022-08-31 20:04:17 +0100

    isogram complete

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,11 @@
 const IsogramFinder = function (word) {
+    this.letters = word.toLowerCase().split("")
 
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
-}
+const filteredList = this.letters.filter((letter,index,array)=> array.indexOf(letter)===index)
+return filteredList.length === this.letters.length
+    }
 
 module.exports = IsogramFinder;
